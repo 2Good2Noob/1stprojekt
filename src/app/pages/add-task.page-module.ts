380a3626eb9fa242cfbe5b889e4bbdd7c09403e0
arturito1994 <artur7632@gmail.com>
2022-04-14 19:07:04 +0200

    .

--- src/app/pages/add-task.page-module.ts
+++ src/app/pages/add-task.page-module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AddTaskPage } from './add-task.page';
 import { TaskListComponentModule } from '../../../projects/task/src/lib/adapters/primary/ui/task-list.component-module';
-import { FirebaseTaskServiceModule } from '../../../projects/task/src/lib/adapters/secondary/infrastructure/firebase-task.service-module';
+import { FirebaseTaskServiceModule } from 'projects/task/src/public-api';
 @NgModule({
   imports: [
     CommonModule,
