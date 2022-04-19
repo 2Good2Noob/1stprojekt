f781087dcaa3e09dadedd24d7ac3cc52f0d4f1c6
arturito1994 <artur7632@gmail.com>
2022-04-20 00:26:18 +0200

    Add Task , Task List

--- projects/task/src/public-api.ts
+++ projects/task/src/public-api.ts
@@ -3,3 +3,4 @@
  */
 export * from './lib/adapters/primary/ui/task-list.component-module';
 export * from './lib/adapters/secondary/infrastructure/firebase-task.service-module';
+export * from './lib/adapters/primary/ui/add-task-list.component-module';
