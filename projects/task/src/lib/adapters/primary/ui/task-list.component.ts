e02e33a65a5584fece36bac0243a3f0f98794cef
arturito1994 <artur7632@gmail.com>
2022-04-21 00:40:32 +0200

    .

--- projects/task/src/lib/adapters/primary/ui/task-list.component.ts
+++ projects/task/src/lib/adapters/primary/ui/task-list.component.ts
@@ -11,10 +11,6 @@ import {
   GetsAllTaskDtoPort,
 } from '../../../application/ports/secondary/gets-all-task.dto-port';
 import { FormGroup, FormControl } from '@angular/forms';
-import {
-  ADDS_TASK_DTO,
-  AddsTaskDtoPort,
-} from '../../../application/ports/secondary/adds-task.dto-port';
 
 @Component({
   selector: 'lib-task-list',
@@ -24,19 +20,8 @@ import {
 })
 export class TaskListComponent {
   task$: Observable<TaskDTO[]> = this._getsAllTaskDto.getAll();
-  readonly addTask: FormGroup = new FormGroup({ zadanie: new FormControl() });
 
   constructor(
-    @Inject(GETS_ALL_TASK_DTO) private _getsAllTaskDto: GetsAllTaskDtoPort,
-    @Inject(ADDS_TASK_DTO) private _addsTaskDto: AddsTaskDtoPort
+    @Inject(GETS_ALL_TASK_DTO) private _getsAllTaskDto: GetsAllTaskDtoPort
   ) {}
-
-  onSubmitAddtasked(addTask: FormGroup): void {
-    if (addTask.invalid) {
-      return;
-    }
-    this._addsTaskDto.add({
-      zadanie: addTask.get('zadanie')?.value,
-    });
-  }
 }
